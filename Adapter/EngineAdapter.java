96c2f9f4e937e04d4ac3016e69ccbde0acdad2f7
MessineseGianluca <gianluca.messi1@gmail.com>
2018-09-21 19:23:47 +0200

    Write main

--- Adapter/EngineAdapter.java
+++ Adapter/EngineAdapter.java
@@ -1,16 +1,16 @@
-package switch;
+import external_library.Engine;
 
 public class EngineAdapter extends SwitchClient {
   private Engine adaptee;
-  
+
   public EngineAdapter(Engine adaptee) {
     this.adaptee = adaptee;
   }
-  
+
   public void turnOn() {
     adaptee.on();
   }
-  
+
   public void turnOff() {
     adaptee.off();
   }
