87e6f756b06393e9080c806f4ecf166753ea17b4
RanaAlessandro <alexhey14@gmail.com>
2018-09-21 18:22:02 +0200

    Fix indentation

--- Adapter/external_library/Engine.java
+++ Adapter/external_library/Engine.java
@@ -1,21 +1,21 @@
 public class Engine {
-    private boolean state;
-    private String name;
+  private boolean state;
+  private String name;
     
-    public Engine(String name) {
-      this.name = name;
-      off();
-    }
-    
-    public void on() {
-      state = true;
-    }
-    
-    public void off() {
-      state = false;
-    }
-    
-    public boolean getState() {
-      return state;
-    }
+  public Engine(String name) {
+    this.name = name;
+    off();
   }
+    
+  public void on() {
+    state = true;
+  }
+    
+  public void off() {
+    state = false;
+  }
+    
+  public boolean getState() {
+    return state;
+  }
+}
