6322d610bd8155f2b2c568510b29135b90f7023d
MessineseGianluca <gianluca.messi1@gmail.com>
2018-09-21 18:29:53 +0200

    Add getName() to engine

--- Adapter/external_library/Engine.java
+++ Adapter/external_library/Engine.java
@@ -1,21 +1,25 @@
 public class Engine {
   private boolean state;
   private String name;
-    
+
   public Engine(String name) {
     this.name = name;
     off();
   }
-    
+
   public void on() {
     state = true;
   }
-    
+
   public void off() {
     state = false;
   }
-    
+
   public boolean getState() {
     return state;
+  }
+
+  public String getName() {
+    return name;
   }
 }
