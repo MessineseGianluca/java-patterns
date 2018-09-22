9827a853998151084d6cea0d3098d96d57060320
MessineseGianluca <gianluca.messi1@gmail.com>
2018-09-22 16:16:45 +0200

    Set proper response for TimeRequest methods

--- Command/receiver/TimeRequest.java
+++ Command/receiver/TimeRequest.java
@@ -7,15 +7,15 @@ public class TimeRequest {
   public static String getCountryFormattedTime(String continent, String capital) {
     String[] fields = { "formatted" };
     String res = formatURLAndSendRequest(continent, capital, fields);
-    System.out.println(res);
-    return res;
+    String value = getFieldValue(res, fields[0]);
+    return "Time for " + capital + ": " + value;
   }
 
   public static String getCountryTimeZoneAbbreviation(String continent, String capital) {
     String[] fields = { "abbreviation" };
     String res = formatURLAndSendRequest(continent, capital, fields);
-    System.out.println(res);
-    return res;
+    String value = getFieldValue(res, fields[0]);
+    return "Timezone Abbreviation for " + capital + ": " + value;
   }
 
   private static String formatURLAndSendRequest(String continent, String capital, String[] fields) {
@@ -30,6 +30,24 @@ public class TimeRequest {
     } catch (Exception e) {
       System.out.println(e);
     }
+
     return res;
+  }
+
+  private static String getFieldValue(String wholeString, String field) {
+    String value = "";
+    int fieldPosition = wholeString.indexOf(field);
+
+    // it considers ":" after field name
+    int valuePosition = fieldPosition + field.length() + 3;
+
+    int i = valuePosition;
+
+    while (wholeString.charAt(i) != '\"') {
+      value += wholeString.charAt(i);
+      i++;
+    }
+
+    return value;
   }
 }
