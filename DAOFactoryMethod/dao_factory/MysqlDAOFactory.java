217cae36da7cb042503c6b27e762af737780a0f4
RanaAlessandro <alexhey14@gmail.com>
2018-09-25 19:01:46 +0200

    Implements DB connection logic

--- DAOFactoryMethod/dao_factory/MysqlDAOFactory.java
+++ DAOFactoryMethod/dao_factory/MysqlDAOFactory.java
@@ -2,13 +2,38 @@ package dao_factory;
 
 import footballer.*;
 import football_team.*;
+import java.sql.*;
 
 public class MysqlDAOFactory extends DAOFactory {
-    public FootballerDAO getFootballerDAO() {
-      return new MysqlFootballerDAO();
+  private static final String DBMS = "jdbc:mysql";
+  private static final String SERVER = "localhost";
+  private static final String DATABASE = "Footballers";
+  private static final String PORT = "3306";
+  private static final String USER_ID = "root";
+  private static final String PASSWORD = "";
+
+  String DRIVER_CLASS_NAME = "org.gjt.mm.mysql.Driver";
+  
+  private static Connection conn = null;
+  
+  public static Connection getConnection() {
+    try {
+      String url = DBMS + "://" + SERVER + ":" + PORT + "/" + DATABASE + "?user=" + USER_ID + "&password=" + PASSWORD;
+      Class.forName("com.mysql.jdbc.Driver");
+      conn = DriverManager.getConnection(url);
+    } catch (SQLException sqlE) {
+      sqlE.printStackTrace();
+    } catch (ClassNotFoundException classE) {
+      classE.printStackTrace();
     }
-    
-    public FootballTeamDAO getFootballTeamDAO() {
-      return new MysqlFootballTeamDAO();
-    }
+    return conn;
   }
+  
+  public FootballerDAO getFootballerDAO() {
+    return new MysqlFootballerDAO();
+  }
+    
+  public FootballTeamDAO getFootballTeamDAO() {
+    return new MysqlFootballTeamDAO();
+  }
+}
